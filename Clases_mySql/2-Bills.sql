ae34503eae198d1de8ab0d0a4f7c42ced0d6abad
EDCA <eric.cavanzo@outlook.com>
2025-04-22 19:36:37 -0500

    Fin base de tablas

--- Clases_mySql/2-Bills.sql
+++ Clases_mySql/2-Bills.sql
@@ -8,11 +8,11 @@ CREATE TABLE IF NOT EXISTS bills (
     FOREIGN KEY (client_id) references clients(client_id)
 );
 
-INSERT INTO bills(client_id, total) values (10, 15.00); //> ERROR 1452 (23000): Cannot add or update 
-a child row: a foreign key constraint fails (`Prueba_Clase_Platzi1`.`bills`, CONSTRAINT `bills_ibfk_1` F
+INSERT INTO bills(client_id, total) VALUES (10, 15.00); //> ERROR 1452 (23000): Cannot add or update 
+a child row: a FOREIGN key constraint fails (`Prueba_Clase_Platzi1`.`bills`, CONSTRAINT `bills_ibfk_1` F
 OREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`))
 
-INSERT INTO clients(client_id, name, email) values (10, 'eduadro', 'eduardo@mail.com'); //> OK
+INSERT INTO clients(client_id, name, email) VALUES (10, 'eduadro', 'eduardo@mail.com'); //> OK
 
 CREATE TABLE IF NOT EXISTS bills (
     bill_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -20,8 +20,5 @@ CREATE TABLE IF NOT EXISTS bills (
     total FLOAT,
     status ENUM('open', 'paid', 'lost') NOT NULL DEFAULT 'open',
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (client_id) references clients(client_id)
-        on DELETE CASCADE 
-        on UPDATE CASCADE
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
---
+++ Clases_mySql/4-pruebas.sql
@@ -0,0 +1,4 @@
+INSERT INTO clients(client_id, name, email) VALUES (10, 'eduardo', 'eduardo@mail.com');
+INSERT INTO products(name, slug) VALUES ('cuaderno', 'slug-cuaderno');
+INSERT INTO bills(client_id, total) VALUES(10, 15.00);
+INSERT INTO bill_products(product_id, bill_id) VALUES ()
